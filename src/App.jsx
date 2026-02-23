d8a3e4f2ba33cdddb25f6f90b764112770ba1b05
Sahil Kumar <sahilkumarrrr27@gmail.com>
2026-02-23 11:26:32 +0530

    dock file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import "./app.scss"
+import Dock from './components/Dock'
+
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <main>
+      <Dock />
+    </main>
   )
 }
 
