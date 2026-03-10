75fc620ba5ea9382a1f721e5c59d59f1c53d3433
Sahil Kumar <sahilkumarrrr27@gmail.com>
2026-03-10 11:10:48 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./app.scss";
 import Dock from "./components/Dock";
-import Nav from "./components/nav";
+import Nav from "./components/Nav";
 import Github from "./components/windows/Github";
 import Notes from "./components/windows/Notes";
 import Resume from "./components/windows/Resume";
